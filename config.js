c5fa8c072e21c0fb28cb7a0bc28d7f2aece3b79e
anfeb <88554978+anfeb@users.noreply.github.com>
2021-09-09 16:58:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
-global.owner = ['6283128734012', '6288709645535'] // Letakan nomor kamu disini
-global.mods = [] // Moderator?
+global.owner = ['6287772343860'// Letakan nomor kamu disini
+global.mods = ['6287772343860'] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -28,8 +28,8 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = '❤️'
-global.author = 'ariffb'
+global.packname = 'Sticker Bot'
+global.author = 'anfebn'
 
 global.wait = '_*tunggu..*_'
 global.eror = '_*Server Error*_'
