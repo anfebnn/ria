0a17b63d7997a93aca045dec9634d06c82e2dd54
anfeb <88554978+anfeb@users.noreply.github.com>
2021-09-09 17:07:02 +0700

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 contoh:
 ${usedPrefix + command} tutup
 ${usedPrefix + command} buka
-	`.trim(), '© stikerin', 'Buka', ',grup 1', 'Tutup', ',grup 0')
+	`.trim(), '@anfebn', 'Buka', ',grup 1', 'Tutup', ',grup 0')
 		throw false
 	}
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
