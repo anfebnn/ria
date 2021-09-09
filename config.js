c08984d361e4a9437b0eba0ded3479c3ea9580d5
anfeb <88554978+anfeb@users.noreply.github.com>
2021-09-09 21:14:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
-global.owner = ['6287772343860'// Letakan nomor kamu disini
-global.mods = [] // Moderator?
+global.owner = ['6287772343860']// Letakan nomor kamu disini
+global.mods = ['6287772343860'] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // nama: 'https://website'
